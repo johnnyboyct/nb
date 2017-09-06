07d746ea902d7a622db96366da5581f06d9c488f
John Marczak <johnnyboyct@hotmail.com>
2017-09-05 22:05:00 -0400

    Polymer

--- src/components/nb-incentives/nb-incentives.js
+++ src/components/nb-incentives/nb-incentives.js
@@ -18,7 +18,9 @@ Polymer({
     },
     google_maps_api_key: {
       type: String,
-      value: 'AIzaSyC36Dibv-Xy2F5Uuvz3DuWQQuNVsiIvlTE',
+      reflectToAttribute: true,
+      notify: true,
+      value: '',
     },
     address: {
       type: String,
@@ -41,13 +43,29 @@ Polymer({
       notify: true,
       //observer: '_handleAjaxEventfunction'
     },
+    nrel_api_key: {
+      type: String,
+      reflectToAttribute: true,
+      notify: true,
+      value: '',
+    },
     electricityAPI: {
       type: Object,
       computed: 'computeFullName(data)',
+      reflectToAttribute: true,
+      notify: true,
       value: {
         baseUrl: "https://developer.nrel.gov/api/pvwatts/v5.json",
-        key: "B3eeEuXR7zVF7Hw8zkUmwp6tv7MEBrlunhW6ny49"
+        key: "B3eeEuXR7zVF7Hw8zkUmwp6tv7MEBrlunhW6ny49",
+        gotResponse: false,
+        apiResponse:{}
       }
+    },
+    gotAPIResponse:{
+      type:Boolean,
+      value: false,
+      reflectToAttribute: true,
+      notify: true
     },
     surface_area:{
       type:Number,
@@ -68,68 +86,66 @@ Polymer({
     tilt: {
       type: String,
       reflectToAttribute: true,
-      value: '',
+      value: '20',
     },
     azimuth: {
       type: String,
       reflectToAttribute: true,
-      value: '',
+      value: '100',
+    },
+    ppa:{
+      type: String,
+      reflectToAttribute: true,
+      notify: true,
+      value: '0.15'
     },
     natureArea:{
       type:Object
     },
-  },
-
-  _handleLatLong: function(el) {
-    //this.$.energy.params.lat = this.latitude;
-    //this.$.energy.params.lon = this.longitude;
-    if (this.latitude && this.longitude) {
-      //this.$.energy.generateRequest();
-      //this.prepAJAXAndSend();
-      this._calculateValue();
+    geoResponse:{
+      type: Boolean,
+      value:false
+    },
+    geoResponseError:{
+      type: String,
+      value: false,
+      reflectToAttribute: true,
+      notify:true
+    },
+    apiResponse:{
+      type:Object,
+      value:{
+      }
+    },
+    mapInitialized:{
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
+      notify: true
+    },
+    readyToCalculate:{
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
+      notify: true
     }
-    console.log(el);
   },
 
-  _addressChanged: function(addr, old) {
-    console.log(addr);
-  },
+  listeners: {
+        //  'tap': 'initialize',
+        //  'tap': '_calculateValue',
+         'geo-error': '_geoError',
+         'geo-response': '_geoResponse',
+         'toggleSection': 'toggleSection'
+       },
 
-  prepAJAXAndSend: function() {
-    this.electricityAPI.url = this.electricityAPI.baseUrl + "?api_key=" + this.electricityAPI.key + "&lat=" + this.latitude + "&lon=" + this.longitude + "&timeframe=monthly&system_capacity=" + this.size_premimum + "&module_type=1&losses=14&array_type=1&tilt=" + this.tilt + "&azimuth=" + this.azimuth + "&callback=?";
-    this.$.energy.url = this.electricityAPI.url;
-    this.$.energy.generateRequest();
-
-  },
-
-
-  _handleAjaxEventfunction: function(el) {
-    var id = event.currentTarget.id;
-    //this.$[id].params.lat = this.latitude;
-    //this.$[id].params.lon = this.longitude;
-    this.notifyPath(id);
-    console.log('ajax',this.$[id]);
-  },
-
-
-  _handleAjaxError: function(el, response) {
-    console.log('error',el);
-  },
-
-
-  iconForItem: function(item) {
-    return item ? (item.integer < 50 ? 'star-border' : 'star') : '';
-  },
-  getClassForItem: function(item, selected) {
-    return selected ? 'item expanded' : 'item';
-  },
   /**
    * Called when the element has been created, but before property values
    * are set and local DOM is initialized. Use for one-time set-up
    * before property values are set.
    **/
   created: function() {
-
+    //console.log('created');
   },
 
   /**
@@ -142,86 +158,202 @@ Polymer({
     // mapsAPI.addEventListener('api-load', function(e) {
     //     this.initialize()
     // });
-    console.log(this);
-
+    ////console.log(this);
+    this.showErrorToast('ready');
   },
 
   _mapsLoaded: function() {
-    console.log(this);
-    this.initialize();
+    ////console.log(this);
+    //console.log('_mapsLoaded');
+    this.initMap();
   },
 
-  initialize: function() {
-    var geocoder = new google.maps.Geocoder();
-    address = this.$.address;
 
-    if (address) {
-      address = address.value;
-      var mapOptions = {
-        zoom: 40,
-        disableDefaultUI: true,
-        zoomControl: true,
-        panControl: true,
-        mapTypeControl: true,
-        scaleControl: true,
-        streetViewControl: true,
-        overviewMapControl: true,
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        tilt: 0,
-      };
-      geocoder.geocode({
-        'address': address,
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          x = results[0].geometry.location;
-        }
-      });
+  _handleLatLong: function(el) {
+    //this.$.energy.params.lat = this.latitude;
+    //this.$.energy.params.lon = this.longitude;
+    if (this.latitude && this.longitude) {
+      //if(!this.geoResponse){
+        this.$.location.show();
+      //}else{
+        //this.$.location.hide();
+      //}
 
-      var map = new google.maps.Map(this.$.mapDiv, mapOptions);
-      var mapOptions45 = {
-        zoom: 20,
-        minZoom: 18,
-        disableDefaultUI: true,
-        zoomControl: true,
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        tilt: 45,
-      };
+      this.map.setCenter({lat: this.latitude, lng: this.longitude});
+      this.initialize();
 
-      geocoder.geocode({
-        'address': address,
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map45.setCenter(results[0].geometry.location);
-          x = results[0].geometry.location;
-        }
-      });
-
-      var map45 = new google.maps.Map(this.$.mapDiv45, mapOptions45);
-      var mapOptionsImage = {
-        zoom: 20,
-        disableDefaultUI: true,
-        zoomControl: false,
-        draggable: false,
-        mapTypeId: google.maps.MapTypeId.SATELLITE,
-        tilt: 45,
-      };
-
-      geocoder.geocode({
-        'address': address,
-      }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          mapImage.setCenter(results[0].geometry.location);
-          x = results[0].geometry.location;
-        }
-      });
-
-      var mapImage = new google.maps.Map(this.$.mapDivImage, mapOptionsImage);
-      this.addButtons(map);
-      this.drawEditablePolygon(map);
+      //this.$.energy.generateRequest();
+      //this.prepAJAXAndSend();
+      //this.initialize();
     }
+    ////console.log(el);
+    //console.log('_handleLatLong');
+  },
+
+  _addressChanged: function(addr, old) {
+    ////console.log(addr);
+  },
+  _geoError: function(event, error) {
+    //console.log(this);
+    this.$.location.show();
+    this.geoResponse = false;
+    this.geoResponseError = error;
+  },
+
+  _geoResponse: function(event, response) {
+    //console.log('_geoResponse');
+    this.geoResponse = true;
+  },
+
+  prepAJAXAndSend: function() {
+    this.electricityAPI.key = this.nrel_api_key;
+    this.electricityAPI.url = this.electricityAPI.baseUrl + "?api_key=" + this.electricityAPI.key + "&lat=" + this.latitude + "&lon=" + this.longitude + "&timeframe=monthly&system_capacity=" + this.size_premimum + "&module_type=1&losses=14&array_type=1&tilt=" + this.tilt + "&azimuth=" + this.azimuth ;
+    this.$.energy.url = this.electricityAPI.url;
+    this.$.energy.generateRequest();
+
+  },
+  _handleAjaxLoading: function () {
+    //console.log('_handleAjaxLoading');
+  },
+
+  _handleAjaxEventfunction: function(event, response) {
+    var id = event.currentTarget.id;
+    var data = event.detail.response;
+    //this.$[id].params.lat = this.latitude;
+    //this.$[id].params.lon = this.longitude;
+    this.notifyPath(id);
+    this.$.response.show();
+    this.$.location.hide();
+    //console.log(response);
 
 
+      var energyGenerated = data.outputs.ac_annual;
+      var lease = energyGenerated * 0.15 * this.ppa;
+
+
+      this.electricityAPI.apiResponse.energyGenerated = energyGenerated.toFixed(2);
+      this.electricityAPI.apiResponse.lease =lease.toFixed(2);
+      this.electricityAPI.apiResponse.address =this.address;
+      this.gotAPIResponse = true;
+
+    //console.log('ajax',this.$[id]);
+  },
+
+  _handleAPIResponse: function (data) {
+    //console.log(data);
+      this.energy = data.detail.response;
+  },
+  _handleAjaxError: function(el, response) {
+    //console.log('error',el);
+    var message = '';
+    if(response.request.response.errors[0]){
+      message = response.request.response.errors[0];
+    }else{
+      message = el.detail.error.message;
+    }
+    this.showErrorToast(message);
+  },
+
+  showErrorToast: function(text, properties) {
+    this.$.toasts.showErrorToast(text, properties);
+  },
+  iconForItem: function(item) {
+    return item ? (item.integer < 50 ? 'star-border' : 'star') : '';
+  },
+  getClassForItem: function(item, selected) {
+    return selected ? 'item expanded' : 'item';
+  },
+
+
+
+
+  initMap: function() {
+    var mapOptions = {
+      zoom: 40,
+      disableDefaultUI: true,
+      zoomControl: true,
+      panControl: true,
+      mapTypeControl: true,
+      scaleControl: true,
+      streetViewControl: true,
+      overviewMapControl: true,
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      tilt: 0,
+    };
+    if(this.latitude && this.longitude){
+      mapOptions.center = {lat: this.latitude, lng: this.longitude};
+    }else{
+      mapOptions.center = {lat: 40.731, lng: -73.997};
+    }
+    this.map = new google.maps.Map(this.$.mapDiv, mapOptions);
+    this.geocoder = new google.maps.Geocoder;
+    this.infowindow = new google.maps.InfoWindow;
+  },
+
+
+  geocodeLatLng: function() {
+    var self = this;
+    //var input = document.getElementById('latlng').value;
+    //var latlngStr = input.split(',', 2);
+    var latlng = {lat: parseFloat(this.latitude), lng: parseFloat(this.longitude)};
+    self.geocoder.geocode({'location': latlng}, function(results, status) {
+      if (status === 'OK') {
+        if (results[0]) {
+          //self.map.setZoom(11);
+          self.address = results[0].formatted_address;
+          // var marker = new google.maps.Marker({
+          //   position: latlng,
+          //   map: self.map
+          // });
+          self.infowindow.setContent(results[0].formatted_address);
+          //self.infowindow.open(self.map, marker);
+          self.map.setCenter(results[0].geometry.location);
+          self.drawEditablePolygon(self.map);
+        } else {
+          //console.log('No results found');
+        }
+      } else {
+        //console.log('Geocoder failed due to: ' + status);
+      }
+    });
+  },
+
+  codeAddress: function() {
+    var self = this;
+    self.geocoder.geocode( { 'address': self.address}, function(results, status) {
+      if (status == 'OK') {
+        self.map.setCenter(results[0].geometry.location);
+        // var marker = new google.maps.Marker({
+        //     map: self.map,
+        //     position: results[0].geometry.location
+        // });
+        self.latitude = results[0].geometry.location.lat();
+        self.longitude = results[0].geometry.location.lng();
+        self.drawEditablePolygon(self.map);
+      } else {
+        //console.log('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  },
+
+
+    initialize: function() {
+    //var geocoder = new google.maps.Geocoder();
+
+
+    if(!this.mapInitialized){
+      if(!this.latitude && !this.longitude){
+        this.codeAddress();
+        //console.log('codeAddress');
+      }else{
+        this.geocodeLatLng();
+        //console.log('geocodeLatLng');
+      }
+  }
+
+    this.addButtons(this.map);
+    //this.drawEditablePolygon(this.map);
+    this.mapInitialized = true;
   },
   addButtons: function(map) {
     this.$.btnTerrain.addEventListener('click', function() {
@@ -248,25 +380,25 @@ Polymer({
   drawEditablePolygon: function(map) {
     var self = this;
     geocoder = new google.maps.Geocoder();
-    address = this.$.address.value;
-    geocoder.geocode({
+    address = this.address;
+    self.geocoder.geocode({
       'address': address,
     }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
-        lat = results[0].geometry.location.lat();
-        lon = results[0].geometry.location.lng();
-        lat_adjacent = Number(results[0].geometry.location.lat()) + 0.0001;
-        lon_adjacent = Number(results[0].geometry.location.lng()) + 0.0001;
-        console.log('LAT: ' + lat + ' | ADJACENT LAT: ' + lat_adjacent + ' LON: ' + lon + ' | ADJACENT LON: ' + lon_adjacent);
+        self.latitude = results[0].geometry.location.lat();
+        self.longitude = results[0].geometry.location.lng();
+        self.lat_adjacent = Number(results[0].geometry.location.lat()) + 0.0001;
+        self.lon_adjacent = Number(results[0].geometry.location.lng()) + 0.0001;
+        //console.log('LAT: ' + self.latitude + ' | ADJACENT LAT: ' + self.latitude + ' LON: ' + self.longitude + ' | ADJACENT LON: ' + self.lon_adjacent);
         self.natureCoords = [
-          new google.maps.LatLng(lat, lon_adjacent),
-          new google.maps.LatLng(lat, lon),
-          new google.maps.LatLng(lat_adjacent, lon),
-          new google.maps.LatLng(lat_adjacent, lon_adjacent),
+          new google.maps.LatLng(self.latitude, self.lon_adjacent),
+          new google.maps.LatLng(self.latitude, self.longitude),
+          new google.maps.LatLng(self.lat_adjacent, self.longitude),
+          new google.maps.LatLng(self.lat_adjacent, self.lon_adjacent),
         ];
         self.natureArea = new google.maps.Polygon({
-          path: natureCoords,
+          path: self.natureCoords,
           strokeColor: '#FFFFFF',
           strokeOpacity: 0.8,
           strokeWeight: 2,
@@ -280,24 +412,31 @@ Polymer({
       }
     });
   },
-  _onAjaxResponse: function(el, re) {
-    console.log(re);
+  _onAjaxResponse: function(request) {
+    //console.log(request.detail.response)
+    this.electricityAPI.gotResponse = true;
+    this.toggleCollapsibleSection('response');
   },
   _calculateValue: function() {
 
     this.surface_area = Number(google.maps.geometry.spherical.computeArea(this.natureArea.getPath()));
     this.size_premimum = Number(this.surface_area * 0.15);
     this.size_standard = Number(this.surface_area * 0.10);
-    document.getElementById('results').innerHTML = "<br><br>RESULTS:<br><br>";
-    document.getElementById('results').innerHTML += "Surface Area = " + this.surface_area.toFixed(0) + " sq meter <br/>";
-    document.getElementById('results').innerHTML += "System size by using premium solar panels (SunPower) = " + this.size_premimum.toFixed(2) + " kW <br/>";
-    document.getElementById('results').innerHTML += "System size by using standard solar panels = " + this.size_standard.toFixed(2) + " kW <br/>";
-    this.tilt = parseInt(document.getElementById('tilt').value);
-    this.azimuth = parseInt(document.getElementById('azimuth').value);
+    //document.getElementById('results').innerHTML = "<br><br>RESULTS:<br><br>";
+
+    this.electricityAPI.apiResponse.surface_area = this.surface_area.toFixed(0);
+    this.electricityAPI.apiResponse.size_premimum = this.size_premimum.toFixed(2);
+    this.electricityAPI.apiResponse.size_standard =  this.size_standard.toFixed(2);
+    //this.tilt = parseInt(this.$.tilt);
+    //this.azimuth = parseInt(this.$.azimuth);
+    //this.ppa = this.$.ppa;
     //var requestUrl = "https://developer.nrel.gov/api/pvwatts/v5.json?api_key=" + this.nrel_api_key + "&lat=" + lat + "&lon=" + lon + "&timeframe=monthly&system_capacity=" + size_premimum + "&module_type=1&losses=14&array_type=1&tilt=" + tilt + "&azimuth=" + azimuth + "&callback=?";
     this.prepAJAXAndSend();
     // $.getJSON(requestUrl, {}, function(data) {
-    //   console.log(data.outputs);
+    //   //console.log(data.outputs);
+
+
+
     //   var energyGenerated = data.outputs.ac_annual;
     //   var ppa = document.getElementById('ppa').value;
     //   var lease = energyGenerated * 0.15 * ppa;
@@ -313,7 +452,7 @@ Polymer({
     //   var i = 0;
     //   utility = '';
     //   while (data.outputs.utility_info[i]) {
-    //     console.log(data.outputs.utility_info[i].utility_name);
+    //     //console.log(data.outputs.utility_info[i].utility_name);
     //     document.getElementById('results2').innerHTML += data.outputs.utility_info[i].utility_name + "<br />";
     //     utility += data.outputs.utility_info[i].utility_name;
     //     i++;
@@ -322,64 +461,12 @@ Polymer({
     // });
 
   },
-
-  /**
-   * Life cycle function that gets called when the element is
-   * first attached to the DOM
-   **/
-  attached: function() {
-
+  toggleCollapsibleSection: function(section) {
+    this.$[section].toggle();
   },
+  toggleSection: function(event) {
+    ////console.log(event.srcElement.section);
+    this.toggleCollapsibleSection(event.srcElement.dataset.section);
 
-  /**
-   * Called after the element is detached from the document.
-   * Can be called multiple times during the lifetime of an element.
-   * Uses include removing event listeners added in
-   **/
-  detached: function() {
-
-  },
-
-
-  /**
-   * Called when one of the element's attributes is changed.
-   * Use to handle attribute changes that don't correspond
-   * to declared properties.
-   **/
-  attributeChanged: function() {
-
-  },
-
-  /**
-   * A public function  that doesnt do anything
-   *
-   * Just demonstating how to document methods
-   *
-   * @param {object} takes a baz object
-   * @return {null}
-   **/
-  foo: function(baz) {
-    return this._bar();
-  },
-
-  /**
-   * A private that doesnt do anything
-   * Just demonstating how to document private methods
-   *
-   * @return {null}
-   **/
-  _bar: function() {
-    return true;
-  },
-
-  /**
-   * ### Events
-   **/
-
-  /**
-   * Fired when nb-incentives does something
-   *
-   * @event nb-incentives-action
-   **/
-
+  }
 });
